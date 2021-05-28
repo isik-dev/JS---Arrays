ebaa3536e77bbb2d61858a6dfa280582f0f86624
islomnumanov <islom4ik.numanov@gmail.com>
2021-05-28 15:06:45 +0900

    st

--- todo.js
+++ todo.js
@@ -1,9 +1,5 @@
 const todos = ['Go out for dinner with friends', 'Get my car washed', 'Finish the project on python', 'Prepare for tommorows class', 'Sleep']
 
-// Delete the third item
-// Add a new item onto the end
-// Remove the first item from the list
-
 todos.splice(2,1)
 todos.push('Buy MacBook Pro')
 todos.shift()
