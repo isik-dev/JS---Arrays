f9b44ea08fa683d72e213b43177e59a13e82e7e4
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-07 16:25:29 +0900

    revision on arrays

--- notes.js
+++ notes.js
@@ -1,103 +1,135 @@
-// const notes = [{}, {
-//     title: 'My next trip',
-//     body: 'I would like to go to Switzerland'
+// // const notes = [{}, {
+// //     title: 'My next trip',
+// //     body: 'I would like to go to Switzerland'
+// // }, {
+// //     title: 'Habits to work on',
+// //     body: 'Exercise. Eating a bit better'
+// // }, {
+// //     title: 'Office modifications',
+// //     body: 'Get a new seat'
+// // }]
+
+// // //pop method removes a value from the end of our array
+// // console.log(notes.pop())
+
+// // //push method adds a value on to the end of our array
+// // notes.push('My new note') 
+
+// // //shift method removes a value from the start of our array
+// // console.log(notes.shift())
+
+// // //unshift method adds a value to the beginning of our array
+// // notes.unshift('My first note')
+
+
+// // notes.splice(1, 1, 'This is the new second item')
+
+// // notes[2]  = 'This is now a new note three'
+
+
+// // notes.forEach(function (item, index) {
+// //     console.log(index)
+// //     console.log(item)
+// // })
+
+
+// // console.log(notes)
+// // console.log(notes[notes.length - 1])
+// // Counting... 1 
+// // for (let count = 2; count >= 0; count --) {
+// //     console.log(count)
+// // }
+
+// // for (let count = notes.length - 1; count >= 0; count --) {
+// //     console.log(notes[count])
+// // }
+
+
+// // const index = notes.findIndex(function (note, index) {
+// //     console.log(note)
+// //     return note.title === 'Habits to work on'
+// // })
+// // console.log(index)
+
+// const notes = [{
+//     title: 'Python',
+//     body: 'Pycharm'
 // }, {
-//     title: 'Habits to work on',
-//     body: 'Exercise. Eating a bit better'
+//     title: 'JavaScript',
+//     body: 'VSCode'
 // }, {
-//     title: 'Office modifications',
-//     body: 'Get a new seat'
+//     title: 'HTML',
+//     body: 'ATOM'
 // }]
 
-// //pop method removes a value from the end of our array
-// console.log(notes.pop())
 
-// //push method adds a value on to the end of our array
-// notes.push('My new note') 
+// const findNotes = function (notes, query) {
+//     return notes.filter(function (note, index) {
+//         const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
+//         const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
+//         return isTitleMatch || isBodyMatch  
+//     })
+// }
 
-// //shift method removes a value from the start of our array
-// console.log(notes.shift())
+// console.log(findNotes(notes, 'arm'))
 
-// //unshift method adds a value to the beginning of our array
+
+
+
+
+
+
+// // const findNote = function (array, noteTitle) {
+// //     const index = array.findIndex(function (element, index) {
+// //         return element.title.toLowerCase() === noteTitle.toLowerCase()
+// //     })
+// //     return array[index]
+// // }
+
+
+// // console.log(notes.length)
+// // console.log(notes)
+
+// // // console.log(notes.indexOf({}))
+
+// // // let someObject = {}
+// // // let otherObject = someObject
+// // // console.log(someObject === otherObject)
+
+// // const index = notes.findIndex(function (note, index) {
+// //     console.log(note)
+// //     return note.title === 'Python'
+// // })
+// // console.log(index)
+
+
+const notes = ['Note 1 ', 'Note 2', 'Note 3']
+
+// notes.push('My new note')
+// const removedFromEnd = notes.pop()
+
 // notes.unshift('My first note')
+// const removedFromBeginning = notes.shift()
 
+// notes.splice(1, 1)
+// notes.splice(1, 0, 'This is the new second item')
 
-// notes.splice(1, 1, 'This is the new second item')
+notes[2] = 'This is now the new note 3'
 
-// notes[2]  = 'This is now a new note three'
+notes.forEach(function (item, index) {
+    console.log(index)
+    console.log(item)
+})
 
+console.log(notes.length)
+console.log(notes)
 
-// notes.forEach(function (item, index) {
-//     console.log(index)
-//     console.log(item)
-// })
+// Counting... 1
 
-
-// console.log(notes)
-// console.log(notes[notes.length - 1])
-// Counting... 1 
-// for (let count = 2; count >= 0; count --) {
-//     console.log(count)
-// }
-
-// for (let count = notes.length - 1; count >= 0; count --) {
-//     console.log(notes[count])
-// }
-
-
-// const index = notes.findIndex(function (note, index) {
-//     console.log(note)
-//     return note.title === 'Habits to work on'
-// })
-// console.log(index)
-
-const notes = [{
-    title: 'Python',
-    body: 'Pycharm'
-}, {
-    title: 'JavaScript',
-    body: 'VSCode'
-}, {
-    title: 'HTML',
-    body: 'ATOM'
-}]
-
-
-const findNotes = function (notes, query) {
-    return notes.filter(function (note, index) {
-        const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
-        const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
-        return isTitleMatch || isBodyMatch  
-    })
+for (count = 5; count>=0; count--) {
+    console.log(count)
 }
 
-console.log(findNotes(notes, 'arm'))
-
-
-
-
-
-
-
-// const findNote = function (array, noteTitle) {
-//     const index = array.findIndex(function (element, index) {
-//         return element.title.toLowerCase() === noteTitle.toLowerCase()
-//     })
-//     return array[index]
-// }
-
-
-// console.log(notes.length)
-// console.log(notes)
-
-// // console.log(notes.indexOf({}))
-
-// // let someObject = {}
-// // let otherObject = someObject
-// // console.log(someObject === otherObject)
-
-// const index = notes.findIndex(function (note, index) {
-//     console.log(note)
-//     return note.title === 'Python'
-// })
-// console.log(index)
+for (let count = notes.length - 1; count >= 0; count--) {
+    console.log(notes[count])
+}
--- todo.js
+++ todo.js
@@ -1,75 +1,106 @@
-// const todos = ['Go out for dinner with friends', 'Get my car washed', 'Finish the project on python', 'Prepare for tommorows class', 'Sleep']
+// // const todos = ['Go out for dinner with friends', 'Get my car washed', 'Finish the project on python', 'Prepare for tommorows class', 'Sleep']
 
-// todos.splice(2,1)
-// todos.push('Buy MacBook Pro')
-// todos.shift()
-
-
-// console.log(`You have ${todos.length} todos`)
+// // todos.splice(2,1)
+// // todos.push('Buy MacBook Pro')
+// // todos.shift()
 
 
-// todos.forEach(function (item, index) {
-//     console.log(`${index + 1}. ${item}`)
-// })
+// // console.log(`You have ${todos.length} todos`)
 
-// // 1. The first item
-// // 2. The second item
-// // ...
 
-// for (let count = 0; count < todos.length; count ++ ) {
-//     console.log(`${count + 1}. ${todos[count]}`)
+// // todos.forEach(function (item, index) {
+// //     console.log(`${index + 1}. ${item}`)
+// // })
+
+// // // 1. The first item
+// // // 2. The second item
+// // // ...
+
+// // for (let count = 0; count < todos.length; count ++ ) {
+// //     console.log(`${count + 1}. ${todos[count]}`)
+// // }
+
+// // for (let count = todos.length - 1; count >= 0; count --) {
+// //     const num = count + 1
+// //     const index =  todos[count]
+// //     console.log(`${num}. ${index}`)
+// // }
+
+// // 1. Convert array to array of objects -> text, completed
+// todos = [{
+//     text: 'Morning Stretching',
+//     status: 'complete'
+// }, {
+//     text: 'Python',
+//     status: 'complete'
+// }, {
+//     text: 'Grocery Shopping',
+//     status: 'incomplete'
+// }, {
+//     text: 'Cleaning My Room',
+//     status: 'incomplete'
+// }, {
+//     text: 'Look For A New House',
+//     status: 'incomplete'
+// }]
+
+// // 2. Create function to remove a todo by text value
+
+// const deleteTodo = function (array, elementText) {
+//     const index = array.findIndex(function (element, index) {
+//         return element.text.toLowerCase() === elementText.toLowerCase()
+//     })
+//     if (array[index]['status'] === 'complete') {
+//         array.splice(index,1)
+//     }
 // }
 
-// for (let count = todos.length - 1; count >= 0; count --) {
-//     const num = count + 1
-//     const index =  todos[count]
-//     console.log(`${num}. ${index}`)
+// const incompleteTodos = function (array) {
+//     return array.filter(function (array) {
+//         // const isBodyMatch = array.status.toLowerCase().includes('incomplete')
+//         // return isBodyMatch
+//         return array.status === 'complete'
+//     })
 // }
 
-// 1. Convert array to array of objects -> text, completed
-todos = [{
-    text: 'Morning Stretching',
-    status: 'complete'
-}, {
-    text: 'Python',
-    status: 'complete'
-}, {
-    text: 'Grocery Shopping',
-    status: 'incomplete'
-}, {
-    text: 'Cleaning My Room',
-    status: 'incomplete'
-}, {
-    text: 'Look For A New House',
-    status: 'incomplete'
-}]
+// incompleteList = incompleteTodos(todos)
+// console.log(incompleteList)
 
-// 2. Create function to remove a todo by text value
 
-const deleteTodo = function (array, elementText) {
-    const index = array.findIndex(function (element, index) {
-        return element.text.toLowerCase() === elementText.toLowerCase()
-    })
-    if (array[index]['status'] === 'complete') {
-        array.splice(index,1)
-    }
+
+
+
+
+// // deleteTodo(todos, 'Python')
+// // console.log(todos)
+
+
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items -> Todo: walk the dog
+
+
+const todos = ['Buy grocery', 'Do homework', 'Clean the room', 'Workout', 'Read KGB']
+
+todos.splice(2, 1)
+todos.push('Buy coffee')
+todos.shift()
+
+console.log(`You have ${todos.length} todos! `)
+
+// 1. The first item
+// 2. The second item
+
+todos.forEach(function (item, index) {
+    const itemIndex = index + 1
+    console.log(`${itemIndex}. ${item}`) // or we can do: console.log(`${index + 1}. ${item}`)
+})
+
+
+// 1. The first item
+// 2. The second item
+// ...
+
+for (let count = 0; count < todos.length; count++) {
+    console.log(`${count + 1}. ${todos[count]}`)
 }
-
-const incompleteTodos = function (array) {
-    return array.filter(function (array) {
-        // const isBodyMatch = array.status.toLowerCase().includes('incomplete')
-        // return isBodyMatch
-        return array.status === 'complete'
-    })
-}
-
-incompleteList = incompleteTodos(todos)
-console.log(incompleteList)
-
-
-
-
-
-
-// deleteTodo(todos, 'Python')
-// console.log(todos)
